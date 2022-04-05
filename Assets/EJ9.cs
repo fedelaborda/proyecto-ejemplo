d6b4f7e3b587424effeefd60ff487e151b79cfc1
fedelaborda <101400350+fedelaborda@users.noreply.github.com>
2022-04-05 14:11:40 -0300

    how to commit for the test ej9

---
+++ Assets/EJ9.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//9. Realizá un programa que resuelva el siguiente problema:  
+//Tres personas aportan diferente capital a una sociedad.Se desea saber qué porcentaje del total aportó cada una 
+//(indicando nombre y porcentaje) y cuál es el monto del total aportado  por las tres.
+//Ingresar por Inspector el ingreso del capital aportado por cada una de las personas y luego  mostrar lo pedido 
+//en el siguiente formato:  
+//Nombre: capital aportado: $ … , Porcentaje del capital: %.... , Monto total aportado: $ …. 
+
+
+public class EJ9 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string nom1;
+    public string nom2;
+    public string nom3;
+
+    public float capital1;
+    public float capital2;
+    public float capital3;
+
+    float PorcentajeAportado1;
+    float PorcentajeAportado2;
+    float PorcentajeAportado3;
+
+    float montototal;
+
+
+    void Start()
+    {
+
+        Debug.Log("Nombre: " + nom1 + " Capital Aportado $: " + capital1 + " Porcentaje del capital: % " + PorcentajeAportado1 + " Monto total aportado: $ " + montototal);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
